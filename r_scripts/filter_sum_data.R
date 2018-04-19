4e2e337cb48f34e31f94fe47a78107aa04e33560
ricpie <ricpie@gmail.com>
2018-04-19 11:25:59 -0600

    Finished all plots.  Could still add a runs plot...

--- r_scripts/filter_sum_data.R
+++ r_scripts/filter_sum_data.R
@@ -7,12 +7,12 @@ library(dplyr)
 
 #________________________________________________________
 # load field metadata and convert each column to appropriate R class
-filter_sumsarized <- function(sumsarized,metadata,bad_files,HHID_remove,project_name){
+filter_sumsarized <- function(sumsarized,metadata,bad_files,HHID_remove,project_name,stove_codes){
 
     #join with the meta data to get the logger id, field_site, and stove_type
     sumsarized$project_name <- project_name
     sumsarized_filtered <- dplyr::mutate(sumsarized,HHID = substring(filename, 
-                    1, sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[1])+2)) %>%       
+                    1, sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[2])-1)) %>%       
         dplyr::mutate(HHID = if_else(!grepl("Nigeria_AfDB",project_name,ignore.case = TRUE),
                     substring(filename,sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[1])+1, 
                     sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[2])-1),
@@ -21,10 +21,10 @@ filter_sumsarized <- function(sumsarized,metadata,bad_files,HHID_remove,project_
       dplyr::left_join(dplyr::select(metadata, filename, logger_id, location,comments,datetime_placed,datetime_removal,
                                    deployment,enumerator,number_loggers_placed_home,stove_type),
                                     by = "filename") %>%
-    dplyr::mutate(HHID = as.factor(HHID)) %>%# 
-    dplyr::mutate(filename = substring(filename, 
+      dplyr::mutate(HHID = as.factor(HHID)) %>%# 
+      dplyr::mutate(filename = substring(filename, 
                                        sapply(filename, function(x) unlist(gregexpr('/',x,perl=TRUE))[1])+1,100)) %>%
-    dplyr::mutate(file_indices = match(filename, unique(filename))) %>%
+      dplyr::mutate(file_indices = match(fullsumsarizer_filename, unique(fullsumsarizer_filename))) %>%
       dplyr::mutate(group = substring(filename, 
                                      1, sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[1])-1)) %>% 
       dplyr::mutate(stove = substring(filename, 
@@ -34,7 +34,9 @@ filter_sumsarized <- function(sumsarized,metadata,bad_files,HHID_remove,project_
     dplyr::distinct(filename,datetime,stove_temp, .keep_all = TRUE)  %>% #Get rid of duplicate data points from the same file being imported/saved twice
     dplyr::ungroup() %>%
     dplyr::mutate(qc = if_else(grepl(bad_files,fullsumsarizer_filename,ignore.case=TRUE),"bad","ok")) %>%
-    dplyr::mutate(qc = if_else(grepl(HHID_remove,HHID),"bad",qc))
+    dplyr::mutate(qc = if_else(grepl(HHID_remove,HHID),"bad",qc)) %>%
+    dplyr::mutate(stove = as.factor(stove)) %>%
+    dplyr::left_join(dplyr::select(stove_codes,stove,stove_descriptions),by = "stove") 
 
   }
 #________________________________________________________
