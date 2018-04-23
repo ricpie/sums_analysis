f028e68a44491ad612318e9520522bf0ba5b5954
ricpie <ricpie@gmail.com>
2018-04-23 11:37:16 -0600

    Changed figure formats and output data to Excel method (openxlsx)

--- r_scripts/load.R
+++ r_scripts/load.R
@@ -1,3 +1,4 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(plyr, purrr, reshape2, reshape, devtools,data.table, tools, readxl, tcltk, tidyverse, knitr, lubridate, dplyr, magrittr, padr, ggpubr, ggplot2, plotly, MASS, quantreg, DT, shiny, shinyFiles, jsonlite, flexdashboard, rsconnect, readr, ggrepel, gsubfn, anytime)
+pacman::p_load(plyr, purrr, reshape2, reshape, devtools,data.table, tools, readxl, tcltk, tidyverse, knitr, lubridate, dplyr, magrittr, 
+               padr, ggpubr, ggplot2, plotly, MASS, quantreg, DT, shiny, shinyFiles, jsonlite, flexdashboard, rsconnect, readr, ggrepel, gsubfn, anytime,openxlsx)
 
