df8f64d1523270486b190bd7ef682a0191558eba
ricpie <ricpie@gmail.com>
2018-05-10 14:18:02 +0600

    added timestamp support for icddrb

--- r_scripts/fix_timestamp_function.R
+++ r_scripts/fix_timestamp_function.R
@@ -9,8 +9,6 @@ library(grDevices)
 
 fileCleanerTimeStamp <- function(filerun){
   
-  #filerun<-"/Users/ricardopiedrahita/Dropbox/2016 ADB RFP Nigeria/SUMS processing/SUMS analysis R code/r_scripts/TC1_111316.csv"
-  
   #extract the header. Import differently based on it.
   header <- read.csv(filerun, nrow=19, blank.lines.skip=F, header=F, stringsAsFactor=F)
   header1 <- read.csv(filerun, nrow=20, blank.lines.skip=F, header=F, stringsAsFactor=F)
@@ -24,23 +22,26 @@ fileCleanerTimeStamp <- function(filerun){
     headerstart = 19
     header <- read.csv(filerun, nrow=headerstart, blank.lines.skip=F, header=F, stringsAsFactor=F)
     datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric')))
-  
-    } else if  (identical(header$V1[16],"Unit")) { #For cellphone-downloaded ibutton
+    names(datas) = c("Date.Time","Unit","Value")    
+    
+  } else if  (identical(header$V1[16],"Unit")) { #For cellphone-downloaded ibutton
     headerstart = 15
     header <- read.csv(filerun, nrow=headerstart, blank.lines.skip=F, header=F, stringsAsFactor=F)
     datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric')))
+    names(datas) = c("Date.Time","Unit","Value")    
     
   } else if  (identical(header$V2[9],"Unit")) { #For cellphone-downloaded ibutton
     headerstart = 8
     header <- read.csv(filerun, nrow=headerstart, blank.lines.skip=F, header=F, stringsAsFactor=F)
     datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric')))
+    names(datas) = c("Date.Time","Unit","Value")    
     
   } else { #For Wellzion
     datas = tryCatch({ datas<- as.data.table(read.csv(filerun, sep="\"",fileEncoding="UCS-2LE", header=1, stringsAsFactor=F)) %>%
-          dplyr::mutate(Unit = "C") 
-          names(datas) = gsub("Thermocouple..","Thermocouple.",names(datas))
-          dplyr::rename(datas,Date.Time = Timestamp,Value = Thermocouple.C.) %>%
-          dplyr::select("Date.Time","Unit","Value")  
+      dplyr::mutate(Unit = "C") 
+    names(datas) = gsub("Thermocouple..","Thermocouple.",names(datas))
+    dplyr::rename(datas,Date.Time = Timestamp,Value = Thermocouple.C.) %>%
+      dplyr::select("Date.Time","Unit","Value")  
     }, error = function(error_condition) {
       datas<- as.data.table(read.csv(filerun, sep="\"",fileEncoding="UCS-2LE", header=1, stringsAsFactor=F)) %>%
         dplyr::mutate(Unit = "C") 
@@ -48,13 +49,13 @@ fileCleanerTimeStamp <- function(filerun){
       names(datas) = c("Date.Time","Unit","Value")    
       datas
     }, finally={
-          datas<- as.data.table(read.csv(filerun, sep="\"",fileEncoding="UCS-2LE", header=1, stringsAsFactor=F)) %>%
-            dplyr::mutate(Unit = "C") 
-          datas <- datas[,c(4,6,12)]
-          names(datas) = c("Date.Time","Unit","Value")
-          datas
+      datas<- as.data.table(read.csv(filerun, sep="\"",fileEncoding="UCS-2LE", header=1, stringsAsFactor=F)) %>%
+        dplyr::mutate(Unit = "C") 
+      datas <- datas[,c(4,6,12)]
+      names(datas) = c("Date.Time","Unit","Value")
+      datas
     })
-        
+    
     header$V1 = NULL
     datas <- as.data.table(datas)
     
@@ -72,13 +73,13 @@ fileCleanerTimeStamp <- function(filerun){
   if (quantile(datas$Value,.01) < 0) {
     datas$Value <- 40 - datas$Value
   }
-
+  
   #Get the number of unique values in the first date position.
   date_string_start_fun <- function(xx,y) {substring(xx,1,sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[1])-1) }
   date_string_middle_fun <- function(xx,y,start,end) {substring(xx, sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[start])+1
-    ,sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[end])-1) }
+                                                                ,sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[end])-1) }
   date_string_end_fun <- function(xx,y) {substring(xx, sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[2])+1
-                                                          ,sapply(xx, function(x) unlist(gregexpr("\ ",x,perl=TRUE)))-1) }
+                                                   ,sapply(xx, function(x) unlist(gregexpr("\ ",x,perl=TRUE)))-1) }
   
   DT1dash <-length(unique(date_string_start_fun(datas$Date.Time,"-")))#Grab first position of the date string assuming it uses a dash delimiter.
   DT2dash <- length(unique(date_string_middle_fun(datas$Date.Time,"-",1,2)))#Grab middle position of the date string assuming it uses a dash delimiter.
@@ -94,9 +95,9 @@ fileCleanerTimeStamp <- function(filerun){
   colonpresence <- unique((sapply(regmatches(datas[,Date.Time], gregexpr(":", datas[,Date.Time])), length)))
   
   dir.create(path=paste(dirname(filerun),'/Corrected Timestamps',sep=""), showWarnings = FALSE)
-
+  
   options(warn=-1)
-
+  
   #If there are more unique values in DT1 than DT2, then the first value is days. This could be defeated if there is only one day of data. Would also not work if there are a lot of different formats with varying levels of leading zeros.
   newfilename = paste(dirname(filerun),'/Corrected Timestamps/',basename(filerun),sep="")
   if ((dashpresence) & (colonpresence==1) & DT1dash>DT2dash) { #D-M-Y hh:mm
@@ -119,6 +120,16 @@ fileCleanerTimeStamp <- function(filerun){
     cat(paste(header$V1, collapse="\n"), file=newfilename)
     write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
     
+  } else if ((slashpresence) & (colonpresence==2) & DT1slash>=DT2slash)  {  #D/M/Y hh:mm:ss
+    datas[,Date.Time:=dmy_hms(as.character(Date.Time))]
+    cat(paste(header$V1, collapse="\n"), file=newfilename)
+    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
+    
+  } else if ((slashpresence) & (colonpresence==1) & DT1slash>=DT2slash)  {  #D/M/Y hh:mm
+    datas[,Date.Time:=dmy_hm(as.character(Date.Time))]
+    cat(paste(header$V1, collapse="\n"), file=newfilename)
+    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
+    
   } else if ((slashpresence) & (colonpresence==1) & DT2slash>DT1slash)  {  #M/D/Y hh:mm
     datas[,Date.Time:=mdy_hm(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
@@ -136,29 +147,30 @@ fileCleanerTimeStamp <- function(filerun){
   
   #Plot the data 
   plot_names <- gsub(".csv",".png",newfilename)
-
+  
   
   tryCatch({ 
-    datas$Date.Time <- parse_date_time(datas$Date.Time, orders = c("y-m-d HMS","y-m-d HM", "m/d/y HM", "m/d/y HMS"))#,"d/m/y HM","d-m-y HM"))
+    datas2 <- datas
+    datas2$Date.Time <- parse_date_time(datas2$Date.Time, orders = c("y-m-d HMS","y-m-d HM", "m/d/y HM", "m/d/y HMS"))#,"d/m/y HM","d-m-y HM"))
     
     #Prepare some text for looking at the ratios of high to low temps.
-    percentiles <- quantile(datas$Value,c(.05,.95))
+    percentiles <- quantile(datas2$Value,c(.05,.95))
     quantile_rpd <- unname(100*(percentiles[2]-percentiles[1])/percentiles[2])
     cat_string <- paste("min T(C) = ",as.character(percentiles[1]),
                         ", max T(C) = ",as.character(percentiles[2]),
                         "max/min = ", strtrim(as.character(quantile_rpd),5))
     png(filename=plot_names,width = 550, height = 480, res = 100)
-    plot(datas$Date.Time, datas$Value, main=plot_names,#xaxt = "n",
+    plot(datas2$Date.Time, datas2$Value, main=plot_names,#xaxt = "n",
          type = "p", xlab = cat_string, ylab="Temp (C)",prob=TRUE,cex.main = .6,cex = .5)
-    #axis(1, datas$Date.Time, format(datas$Date.Time, "%m/%d"), cex.axis = .7)
+    #axis(1, datas2$Date.Time, format(datas2$Date.Time, "%m/%d"), cex.axis = .7)
     grid(nx = 5, ny = 10, col = "lightgray", lty = "dotted",
          lwd = par("lwd"), equilogs = TRUE)
     dev.off()
     
   }, error = function(error_condition) {
-
+    
   }, finally={
-
+    
   })
   
 }
