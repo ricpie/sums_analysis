05f0a4592914a1b76350a880087c716b9af6993e
Ricardo Piedrahita <ricardopiedrahita@Ricardos-iMac.domain>
2018-06-06 15:44:16 -0600

    Added a new table and added more custom hhid filtering for afdb project.

--- r_scripts/filter_sum_data.R
+++ r_scripts/filter_sum_data.R
@@ -11,13 +11,24 @@ filter_sumsarized <- function(sumsarized,metadata,bad_files,HHID_remove,project_
   
   #join with the meta data to get the logger id, field_site, and stove_type
   sumsarized$project_name <- project_name
-  sumsarized_filtered <- dplyr::mutate(sumsarized,HHID = substring(filename, 
-                                                                   1, sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[2])-1)) %>%       
+  sumsarized_filtered <-     dplyr::mutate(sumsarized,filename = gsub("KE\\_MUS","MUS",filename)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = substring(filename,1, sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[2])-1)) %>%  
+    dplyr::mutate(filename = if_else(grepl("Nigeria_AfDB",project_name,ignore.case = TRUE) & grepl("ke",HHID,ignore.case=TRUE),
+                                     gsub("ke","_KE",filename,ignore.case = T),filename)) %>% #Special case for the AfDB households 
+    dplyr::mutate(HHID = substring(filename,1, sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[2])-1)) %>%  
     dplyr::mutate(HHID = if_else(!grepl("Nigeria_AfDB",project_name,ignore.case = TRUE),
                                  substring(filename,sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[1])+1, 
                                            sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[2])-1),
                                  HHID)) %>%  
+    dplyr::mutate(HHID = if_else(grepl("\\_KE\\(B\\)",filename,ignore.case=TRUE),gsub("\\_KE\\(B\\)","B\\_KE",HHID),HHID)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = if_else(grepl("\\_KE1\\(B\\)",filename,ignore.case=TRUE),gsub("\\_KE1\\(B\\)","B\\_KE",HHID),HHID)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = if_else(grepl("\\_KE2\\(B\\)",filename,ignore.case=TRUE),gsub("\\_KE2\\(B\\)","B\\_KE",HHID),HHID)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = if_else(grepl("\\_KE\\(1\\)\\(B\\)",filename,ignore.case=TRUE),gsub("\\_KE\\(1\\)\\(B\\)","B_KE",HHID),HHID)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = if_else(grepl("\\_CC\\(B\\)",filename,ignore.case=TRUE),gsub("\\_CC\\(B\\)","B\\_CC",HHID),HHID)) %>% #Special case for the B round of households  
     dplyr::mutate(HHID = if_else(grepl("\\(B\\)",filename,ignore.case=TRUE),gsub("\\(B\\)","B",HHID),HHID)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = if_else(grepl("\\(B\\)",filename,ignore.case=TRUE),paste0(HHID,"B"),HHID)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = gsub("BB","B",HHID)) %>% #Special case for the B round of households  
+    dplyr::mutate(HHID = gsub("Ke","",HHID,ignore.case = T)) %>% #Special case for the B round of households  
     dplyr::left_join(dplyr::select(metadata, filename, logger_id, location,comments,datetime_placed,datetime_removal,
                                    deployment,enumerator,number_loggers_placed_home,stove_type),
                      by = "filename") %>%
@@ -38,7 +49,6 @@ filter_sumsarized <- function(sumsarized,metadata,bad_files,HHID_remove,project_
     dplyr::mutate(qc = if_else(grepl(HHID_remove,HHID),"bad",qc)) %>%
     dplyr::mutate(stove = as.factor(stove)) %>%
     dplyr::left_join(dplyr::select(stove_codes,stove,stove_descriptions),by = "stove") 
-  
   
 }
 #________________________________________________________
