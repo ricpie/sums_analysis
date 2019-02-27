81d032d798e77e6c3081788a82116b14968053d7
ricpie <ricpie@gmail.com>
2019-02-27 11:34:50 -0700

    Added a missing file

---
+++ r_files/parameter_file_ICDDRB.R
@@ -0,0 +1,42 @@
+#Parameter file for analysis
+#Set path to tracking sheet relative to 'SUMS processing' directory
+path_tracking_sheet <- "SUMs_tracking form icddrb.xlsx"
+
+project_name <- "ICDDRB"
+#Text associating a given stove code with the full name to use in figures.
+stove_codes <- data.frame(stove = as.factor(c("TMS","TMT","TMS2","TMT2","LPG","LPG2","FMS","ICS", "AMB")),
+                          stove_descriptions = as.factor(c("1st Traditional Mud Stove, single burger",
+                                                           "1st traditional mud stove, two burner",
+                                                           "Secondary single burner traditional mud stove",
+                                                           "Secondary two burner traditional mud stove",
+                                                           "Liquified petroleum gas",
+                                                           "Secondary Liquified petroleum gas",
+                                                           "Floating mud stove","Local improved wood stove",
+                                                           "Ambient")))
+
+stove_group_codes <- data.frame(group = as.factor(c("Pre","Post")),  #Use these if there are different study arms.
+                                stove_groups = as.factor(c("Pre LPG Intervention","Post LPG Intervention"))) #group variable in filter_sumsarized.R
+
+cooking_group <- 40 # x minute window for grouping events together.
+cooking_duration_minimum <- 9  #Minutes
+cooking_duration_maximum <- 1440 #Minutes
+logging_duration_minimum <- 1 #days.  Single file duration must be this long to be included in the analysis.
+#If set to zero, it will not trim out any data, instead leaving the complete data sets available for analysis.  Trimming is done
+#to account for placement times/dates.
+total_days_logged_minimum <- 5 #Must have at least this many days for a households's stove to be included in the analysis. Default value is 5.
+metadata_date_ignore <- 1
+
+start_date_range <- "2018-5-1" #Year-Month-Day format.  Do not include data before this in the analysis
+end_date_range <- "2019-12-1" #Do not include data after this in the analysis
+
+
+# Remove data from files selected as bad (they match the following strings, so they should be specific to the 
+#given file, not generic enough to remove more files than intended)
+bad_files <- paste(c("Pre_Pilot-04_TMS_DL-01","|Pre_Pilot-04_TMS_DL-01","|Pre_Pilot-04_TMS_DL-01"
+                     ,"|Pre_Pilot-04_TMS_DL-01"),collapse="")
+
+# Exclude data from the following households from the entire analysis. e.g.SHO_03 is removed
+HHID_remove <- paste(c("^SHO_03$","|^SHO_04$","|^MUS_01$"),collapse="")
+
+
+
