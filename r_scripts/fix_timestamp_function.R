f542d66918cacdd7f3f987221b332d0de3addb3a
ricpie <ricpie@gmail.com>
2019-11-19 15:26:01 -0700

    Added support for a new messed up timestamp

--- r_scripts/fix_timestamp_function.R
+++ r_scripts/fix_timestamp_function.R
@@ -16,24 +16,24 @@ fileCleanerTimeStamp <- function(filerun){
   if (identical(header1$V1[21],"Unit")) { #For standard ibutton
     headerstart = 19
     header <- read.csv(filerun, nrow=headerstart, blank.lines.skip=F, header=F, stringsAsFactor=F)
-    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric')))
+    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric'))[ ,1:3])
     
   } else if  (identical(header2$V2[20],"Unit")) { #For cellphone-downloaded ibutton
     headerstart = 19
     header <- read.csv(filerun, nrow=headerstart, blank.lines.skip=F, header=F, stringsAsFactor=F)
-    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric')))
+    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric'))[ ,1:3])
     names(datas) = c("Date.Time","Unit","Value")    
     
   } else if  (identical(header$V1[16],"Unit")) { #For cellphone-downloaded ibutton
     headerstart = 15
     header <- read.csv(filerun, nrow=headerstart, blank.lines.skip=F, header=F, stringsAsFactor=F)
-    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric')))
+    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric'))[ ,1:3])
     names(datas) = c("Date.Time","Unit","Value")    
     
   } else if  (identical(header$V2[9],"Unit")) { #For cellphone-downloaded ibutton
     headerstart = 8
     header <- read.csv(filerun, nrow=headerstart, blank.lines.skip=F, header=F, stringsAsFactor=F)
-    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric')))
+    datas <- as.data.table(read.csv(filerun, skip=headerstart, colClasses=c('character', 'character', 'numeric'))[ ,1:3])
     names(datas) = c("Date.Time","Unit","Value")    
     
   } else { #For Wellzion
@@ -91,74 +91,74 @@ fileCleanerTimeStamp <- function(filerun){
   date_string_middle_fun <- function(xx,y,start,end) {substring(xx, sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[start])+1
                                                                 ,sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[end])-1) }
   date_string_end_fun <- function(xx,y) {substring(xx, sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE))[2])+1
-                                                   ,sapply(xx, function(x) unlist(gregexpr("\ ",x,perl=TRUE)))-1) }
+                                                   ,sapply(xx, function(x) unlist(gregexpr(y,x,perl=TRUE)))-1) }
   
-  DT1dash <-length(unique(date_string_start_fun(datas$Date.Time,"-")))#Grab first position of the date string assuming it uses a dash delimiter.
-  DT2dash <- length(unique(date_string_middle_fun(datas$Date.Time,"-",1,2)))#Grab middle position of the date string assuming it uses a dash delimiter.
-  DT3dash <- length(unique(date_string_end_fun(datas$Date.Time,"-")))#Grab last position of the date string assuming it uses a dash delimiter.
+  DT1dash <-length(unique(date_string_start_fun(datas$Date.Time[1],"-")))#Grab first position of the date string assuming it uses a dash delimiter.
+  DT2dash <- length(unique(date_string_middle_fun(datas$Date.Time[1],"-",1,2)))#Grab middle position of the date string assuming it uses a dash delimiter.
+  DT3dash <- length(unique(date_string_end_fun(datas$Date.Time[1],"-")))#Grab last position of the date string assuming it uses a dash delimiter.
   
-  DT1slash <-length(unique(date_string_start_fun(datas$Date.Time,"/")))#Grab first position of the date string assuming it uses a dash delimiter.
-  DT2slash <- length(unique(date_string_middle_fun(datas$Date.Time,"/",1,2)))#Grab middle position of the date string assuming it uses a dash delimiter.
-  DT3slash <- length(unique(date_string_end_fun(datas$Date.Time,"/")))#Grab last position of the date string assuming it uses a dash delimiter.
+  DT1slash <-length(unique(date_string_start_fun(datas$Date.Time[1],"/")))#Grab first position of the date string assuming it uses a dash delimiter.
+  DT2slash <- length(unique(date_string_middle_fun(datas$Date.Time[1],"/",1,2)))#Grab middle position of the date string assuming it uses a dash delimiter.
+  DT3slash <- length(unique(date_string_end_fun(datas$Date.Time[1],"/")))#Grab last position of the date string assuming it uses a dash delimiter.
   
-  slashpresence <- unique((sapply(regmatches(datas[,Date.Time], gregexpr("/", datas[,Date.Time])), length)))>1
-  dashpresence <- unique((sapply(regmatches(datas[,Date.Time], gregexpr("-", datas[,Date.Time])), length)))>1
+  slashpresence <- unique((sapply(regmatches(datas[1,Date.Time], gregexpr("/", datas[1,Date.Time])), length)))>1
+  dashpresence <- unique((sapply(regmatches(datas[1,Date.Time], gregexpr("-", datas[1,Date.Time])), length)))>1
   #check number of ":" in datetime stamp
-  colonpresence <- unique((sapply(regmatches(datas[,Date.Time], gregexpr(":", datas[,Date.Time])), length)))
+  colonpresence <- unique((sapply(regmatches(datas[1,Date.Time], gregexpr(":", datas[1,Date.Time])), length)))
   
   dir.create(path=paste(dirname(filerun),'/Corrected Timestamps',sep=""), showWarnings = FALSE)
   
   options(warn=-1)
   
   # datas <- dplyr::mutate(datas,Value = if_else(Value < 0,quantile(Value, .05),Value)) #Remove -270 data that sometimes appears due to disconnected Wellzions.
-  
+  datas$Date.TimeBackup = datas$Date.Time
   #If there are more unique values in DT1 than DT2, then the first value is days. This could be defeated if there is only one day of data. Would also not work if there are a lot of different formats with varying levels of leading zeros.
   newfilename = paste(dirname(filerun),'/Corrected Timestamps/',basename(filerun),sep="")
   if ((dashpresence) & (colonpresence==1) & DT1dash>DT2dash) { #D-M-Y hh:mm
     datas[,Date.Time:=dmy_hm(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
-    
   } else if ((dashpresence) & (colonpresence==2) & DT1dash>DT2dash) {  #D-M-Y hh:mm:ss
     datas[,Date.Time:=dmy_hms(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
-    
   } else if ((dashpresence) & (colonpresence==1) & DT3dash>DT2dash) {  #Y-m-d hh:mm
     datas[,Date.Time:=ymd_hm(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file = newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
-    
   } else if ((dashpresence) & (colonpresence==2) & DT3dash>DT2dash)  {  #y-m-d hh:mm:ss
     datas[,Date.Time:=ymd_hms(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
-    
+
   } else if ((slashpresence) & (colonpresence==2) & DT1slash>=DT2slash)  {  #D/M/Y hh:mm:ss
     datas[,Date.Time:=dmy_hms(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
-    
+
   } else if ((slashpresence) & (colonpresence==1) & DT1slash>=DT2slash)  {  #D/M/Y hh:mm
     datas[,Date.Time:=dmy_hm(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
     
   } else if ((slashpresence) & (colonpresence==1) & DT2slash>DT1slash)  {  #M/D/Y hh:mm
     datas[,Date.Time:=mdy_hm(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
-    
+
   } else if ((slashpresence) & (colonpresence==2) & DT2slash>DT1slash)  {  #M/D/Y hh:mm:ss
     datas[,Date.Time:=mdy_hms(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
   } else  {
     datas[,Date.Time:=mdy_hms(as.character(Date.Time))]
     cat(paste(header$V1, collapse="\n"), file=newfilename)
-    write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
   }
   
+  # if there are a bunch of bad dates still, try to fix the rest.
+  if(sum(is.na(datas$Date.Time))>100){
+    a <- datas$Date.Time
+    b <- dmy_hm(as.character(datas$Date.TimeBackup)) # Produces NA when format is not "%d.%m.%Y"
+    a[is.na(a)] <- b[!is.na(b)] # Combine both while keeping their ranks
+    datas$Date.Time <- a # Put it back in your dataframe
+  }
+  datas[,Date.TimeBackup:=NULL]
+  
+  write.table(datas, newfilename, sep=",", append=TRUE, row.names=F, quote=F)
+  
+
   #Plot the data 
   plot_names <- gsub(".csv",".png",newfilename)
   
