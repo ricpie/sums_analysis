4fb8aac2f9a2ba43b3471e920bf5d38b8785c92d
ricpie <ricpie@gmail.com>
2019-11-20 11:02:33 -0700

    parallel wasn't working

--- fix_timestamps_analysis.R
+++ fix_timestamps_analysis.R
@@ -15,8 +15,7 @@ source('r_scripts/functions_thermocouple.R') #For the thermocouple logger used i
 files_plot <- tk_choose.files(default = "", caption = "Select files",
                               multi = TRUE, filters = matrix(c("SUMs files",".csv"),1, 2, byrow = TRUE), index = 1)
 
-
-l_ply(files_plot, fileCleanerTimeStamp, .progress='text',.parallel = TRUE) #Fix timestamps and headers if necessary.
+l_ply(files_plot, fileCleanerTimeStamp, .progress='text') #Fix timestamps and headers if necessary.
 
 #l_ply(files_plot, fileFlip, .progress='text')#File may have timestamp flipped...this reverses it.
 
